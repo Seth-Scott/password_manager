104b594260ac3f774fdb4f1e0c43ee1556cace1d
Seth Scott <s.scottvancouverwa@gmail.com>
2022-03-09 15:50:52 -0800

    add entries for website and username/password

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-PINK = "#e2979c"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -13,5 +12,20 @@ canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=3, column=3)
+
+
+website_label = Label(text="Website:")
+website_label.grid(row=4, column=2)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=4, column=3)
+
+email_usr_label = Label(text="Email/Username:")
+email_usr_label.grid(row=5, column=2)
+
+email_usr_entry = Entry(width=35)
+email_usr_entry.grid(row=5, column=3)
+
+
 
 window.mainloop()
