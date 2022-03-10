864fcf1ed771d239eb826fc17bad3220ac28f0f6
Seth Scott <s.scottvancouverwa@gmail.com>
2022-03-09 19:46:17 -0800

    add random letter/number generator functionality

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, shuffle, randint
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+    letter_list = [choice(letters) for _ in range(randint(8, 10))]
+    symbol_list = [choice(symbols) for _ in range(randint(2, 4))]
+    number_list = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = letter_list + symbol_list + number_list
+    generated_password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, generated_password)
+    pyperclip.copy(generated_password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
 def add_password():
     site = website_entry.get()
     email = email_usr_entry.get()
     password = password_entry.get()
-    passwords = {site: email}
 
     is_ok = messagebox.askokcancel(title=site, message=f"These are the details entered:\nEmail: {email}\n"
                                                f" Password: {password}\n Okay to save?")
@@ -53,7 +71,7 @@ password_label.grid(row=6, column=2)
 password_entry = Entry(width=21)
 password_entry.grid(row=6, column=3, columnspan=2, sticky="W")
 
-generate_button = Button(text="Generate")
+generate_button = Button(text="Generate", command=generate_password)
 generate_button.grid(row=6, column=4)
 
 add_button = Button(text="Add", width=32, command=add_password)
