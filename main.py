b0558529de3c099225d05a7c9a5365e262843e41
Seth Scott <s.scottvancouverwa@gmail.com>
2022-03-12 20:18:33 -0800

    add basic search feature

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from tkinter import messagebox
 from random import choice, randint
 import pyperclip
 import json
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -21,9 +21,8 @@ def generate_password():
     password_entry.insert(0, generated_password)
     pyperclip.copy(generated_password)
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
 
-
+# ---------------------------- SAVE PASSWORD ------------------------------- #\
 def add_password():
     site = website_entry.get()
     email = email_usr_entry.get()
@@ -53,9 +52,19 @@ def add_password():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+
+# ---------------------------- SEARCH ------------------------------- #
+def search():
+    try:
+        with open("data.json", mode="r") as file:
+            data = json.load(file)
+            site_data = str(data[website_entry.get()])
+            messagebox.showinfo(title=website_entry.get(), message=site_data)
+    except:
+        messagebox.showinfo(title=website_entry.get(), message=f'No password saved for site: "{website_entry.get()}"\n ')
+
+
 # ---------------------------- UI SETUP ------------------------------- #
-
-
 window = Tk()
 window.title("MyPass Password Manager")
 window.config(padx=20, pady=20)
@@ -65,31 +74,33 @@ logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=3, column=3)
 
-
 website_label = Label(text="Website:")
 website_label.grid(row=4, column=2)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=4, column=3, columnspan=2)
+website_entry = Entry(width=22)
+website_entry.grid(row=4, column=3, columnspan=2, sticky="W")
 website_entry.focus()
+
+search_button = Button(text="Search", command=search)
+search_button.grid(row=4, column=4)
 
 email_usr_label = Label(text="Email/Username:")
 email_usr_label.grid(row=5, column=2)
 
-email_usr_entry = Entry(width=35)
+email_usr_entry = Entry(width=31)
 email_usr_entry.grid(row=5, column=3, columnspan=2)
 email_usr_entry.insert(0, "user@gmail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(row=6, column=2)
 
-password_entry = Entry(width=21)
+password_entry = Entry(width=22)
 password_entry.grid(row=6, column=3, columnspan=2, sticky="W")
 
 generate_button = Button(text="Generate", command=generate_password)
 generate_button.grid(row=6, column=4)
 
-add_button = Button(text="Add", width=32, command=add_password)
+add_button = Button(text="Add", width=28, command=add_password)
 add_button.grid(row=7, column=3, columnspan=5)
 
 window.mainloop()
